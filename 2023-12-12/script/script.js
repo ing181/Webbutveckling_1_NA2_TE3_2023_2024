38dbdac1d312a92f3611f1ee7fac74371c3dda23
Ingemar Ceicer <Ingemar.Ceicer@lerum.se>
2023-12-13 15:30:30 +0100

    FUNGERAR

--- 2023-12-12/script/script.js
+++ 2023-12-12/script/script.js
@@ -1,13 +1,15 @@
+
+
 var x = document.getElementById("knapp");
 x.addEventListener("click",red);
 function red()
 {
-    document.getElementById("gul").style.backgroundColor("red");
+    document.getElementById("gul").style.backgroundColor="red";
 }
-var  y= document.getElementById("gul");
-y.addEventListener("mouseover",black);
+var x= document.getElementById("gul");
+x.addEventListener("mouseover",black);
 
 function black()
 {
-    document.getElementById("gul").style.backgroundColor("black");
+    document.getElementById("gul").style.backgroundColor="black";
 }
